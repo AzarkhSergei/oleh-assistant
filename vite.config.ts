b3d3b9a3522100ed9aadbc1a971c727b2589c132
AzarkhSergei <sergei.azarkh@gmail.com>
2025-05-21 22:11:33 +0300

    feat: auto-generate sitemap and robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://oleh-assistant.netlify.app',
+      readable: true,
+      generateRobotsTxt: true,
+      dynamicRoutes: [
+        '/step/healthcare',
+        '/step/bank',
+        '/step/housing',
+        '/step/employment',
+        '/step/integration',
+        '/step/education',
+        '/step/taxes',
+        '/step/documents'
+        // Добавь другие страницы, если они динамически создаются
+      ],
+    }),
+  ],
 });
